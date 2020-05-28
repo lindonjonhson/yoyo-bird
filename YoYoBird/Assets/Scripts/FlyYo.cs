6cf954153d64c9971019177a36169a88d567ebb9
lindonjonhson <lindon.jonhsonf@gmail.com>
2020-05-28 13:58:48 +0100

    First unity project in git!
    Just uploading what I've built so far.

---
+++ YoYoBird/Assets/Scripts/FlyYo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyYo : MonoBehaviour
+{
+    public GameManager gameManager;
+    public float velocity = 2;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // The 0 means left click
+        if(Input.GetMouseButtonDown(0)){
+            // Jump
+            rb.velocity = Vector2.up * velocity;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        gameManager.GameOver();
+    }
+}
---
+++ YoYoBird/Assets/Scripts/PipeSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawner : MonoBehaviour
+{
+    public float maxTime = 4;
+    private float timer = 0;
+    public GameObject pipe;
+    public float height;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject newPipe = Instantiate(pipe);
+        // newPipe.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+        newPipe.transform.position = transform.position + new Vector3(0, Random.Range(-0.15f, 0.9f), 0);
+        Destroy(newPipe, 7);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > maxTime){
+            GameObject newPipe = Instantiate(pipe);
+            // newPipe.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+            newPipe.transform.position = transform.position + new Vector3(0, Random.Range(-0.15f, 0.9f), 0);
+            Destroy(newPipe, 7);
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+    }
+}
